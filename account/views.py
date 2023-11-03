e8e097d21accd8251c68a1fb9b8f1e3069a88c03
haroon <haroonasifmalik123@gmail.com>
2023-11-03 17:32:43 +0500

    Login page setup.

--- account/views.py
+++ account/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm, UserProfileInfoForm
 
+from django.contrib.auth import authenticate, login, logout
+# from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/home/')
+            else:
+                return HttpResponse('The user is not active')
+        else:
+            return HttpResponse('No user found')
+
+    return render(request, 'account/login.html', {})
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return HttpResponse('Welcome to the home page!')
+    else:
+        request.session['message'] = 'You need to be logged in to access the home page.'
+        return redirect('user_login')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('register')
+
 
 def register(request):
     registered = False
@@ -10,15 +45,12 @@ def register(request):
         profile_form = UserProfileInfoForm(data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
-            # Create a new user
             user = user_form.save()
             user.set_password(user.password)
             user.save()
 
-            # Create a related UserProfileInfo instance
             profile = profile_form.save(commit=False)
-            profile.user = user  # Link the profile to the user
-
+            profile.user = user
             if 'profile_photo' in request.FILES:
                 profile.profile_photo = request.FILES['profile_photo']
                 profile.save()
@@ -30,8 +62,4 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileInfoForm()
 
-    return render(request, 'account/registeration.html', {'profile_form': profile_form, 'user_form': user_form, 'registered': registered})
-
-
-def login(request):
-    return render(request, 'account/login.html')
+    return render(request, 'account/registration.html', {'profile_form': profile_form, 'user_form': user_form, 'registered': registered})
--- core/settings.py
+++ core/settings.py
@@ -144,6 +144,8 @@ STATICFILES_DIRS = [STATIC_DIR,]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = '/account/login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
